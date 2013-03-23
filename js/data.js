f31dce43d3a8466f52a7b22d6e6eca1ed7718655
Emmanuel Goossaert <emmanuel.goossaert@gmail.com>
2013-03-23 15:56:16 +0100

    Normalized first dataset.

--- js/data.js
+++ js/data.js
@@ -23,7 +23,18 @@ function get_data(x, y, distance, num_rows) {
             console.log('error');
         },
         success: function (data) {
-            console.log(JSON.stringify(data, null, 2));
+            //console.log(JSON.stringify(data, null, 2));
+            var i = 0;
+            data_out = [];
+            for (i = 0; i < data['records'].length; i++) {
+                poi = {'name': data['records'][i]['fields']['nom'],
+                       'geom_x': data['records'][i]['fields']['geom_x_y'][0],
+                       'geom_y': data['records'][i]['fields']['geom_x_y'][1],
+                       'comment': data['records'][i]['fields']['com']
+                      };
+                data_out.push(poi);
+            }
+            console.log(JSON.stringify(data_out, null, 2));
         }
     });
 
